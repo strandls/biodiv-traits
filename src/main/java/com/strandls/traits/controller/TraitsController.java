7392c87f679a3c9cd3762aa94e0b0b3d5faad75e
Abhishek Kumar Rudra <abhishek@darbhanga.strandls.com>
2019-08-22 16:53:36 +0530

    Swagger plugin Updated

--- src/main/java/com/strandls/traits/controller/TraitsController.java
+++ src/main/java/com/strandls/traits/controller/TraitsController.java
@@ -24,16 +24,12 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
 
 /**
  * @author Abhishek Rudra
  *
  */
 @Api("Traits Service")
-@SwaggerDefinition(tags = {
-		@Tag(name = "Traits Serivce to get Facts Value", description = "Rest endpoint for Observatin Service") })
 @Path(ApiConstants.V1 + ApiConstants.FACTSERVICE)
 public class TraitsController {
 
@@ -77,10 +73,9 @@ public class TraitsController {
 	@Path(ApiConstants.IBP + "/{traitId}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
-	
+
 	@ApiOperation(value = "Find Traits by Observation ID for ibp", notes = "Returns the key value pair of Tarits for a particular Observation", response = FactValuePair.class)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Success", response = FactValuePair.class),
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = FactValuePair.class),
 			@ApiResponse(code = 404, message = "Traits not found", response = String.class) })
 
 	public Response getFactIbp(@PathParam("traitId") String traitId) {
